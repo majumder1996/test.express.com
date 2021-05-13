38a09f0b44bf972648670a6e8927c72db97fc608
majumder1996 <titasmajumder1996@gmail.com>
2021-05-13 17:33:59 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const Express = require("express");
+const fs = require("fs");
+
+const server = Express();
+
+const PORT = 3001;
+var logData = "";
+
+
+server.get('/', (req,res) => {
+    res.send("You have hitted a GET API on port: "+PORT);
+    let current = new Date();
+    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Someone hits the GET API at \n";
+    fs.appendFile('./logs/APILogs.log',logData, (err) => {
+        if(err) throw err;
+    });
+    console.log(logData);
+});
+server.post('/', (req,res) => {
+    res.send("You have hitted POST API on port: "+PORT);
+    let current = new Date();
+    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Someone hits the POST API at \n";
+    fs.appendFile('./logs/APILogs.log',logData, (err) => {
+        if(err) throw err;
+    });
+    console.log(logData);
+});
+
+server.put('/', (req,res) => {
+    res.send("You have hitted PUT API on port: "+PORT);
+    let current = new Date();
+    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Someone hits the PUT API at \n";
+    fs.appendFile('./logs/APILogs.log',logData, (err) => {
+        if(err) throw err;
+    });
+    console.log(logData);
+});
+
+server.delete('/', (req,res) => {
+    res.send("You have hitted DELETE API on port: "+PORT);
+    let current = new Date();
+    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Warning! Someone hits the DELETE API at \n";
+    fs.appendFile('./logs/APILogs.log',logData, (err) => {
+        if(err) throw err;
+    });
+    console.log(logData);
+});
+
+server.listen(PORT, () => {
+    console.log("App is now answering to port: "+PORT);
+});
