5497f8b22f7863bf28a11d2a1d700ecb2b940ce4
majumder1996 <titasmajumder1996@gmail.com>
2021-05-18 21:43:43 +0530

    database connection mongodb

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const Express = require("express");
+var MongoClient = require('mongodb').MongoClient;
 const fs = require("fs");
+var BodyParser = require('body-parser');
 
 const server = Express();
+var connectionString = "mongodb://192.168.226.137:27017/expressDB"
+var logData = "";
+var clientDB;
+
+server.use(BodyParser.urlencoded({extended:true}));
+server.use(BodyParser.json());
+
+MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+  }, (err, client) => {
+    if (err) { 
+        return console.error(err);
+    }
+    else {
+        let current = new Date();
+        clientDB = client;
+        logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] App got connected to Database "+connectionString+" \n";
+        fs.appendFile('./logs/APILogs.log',logData, (err) => {
+            if(err) throw err;
+        });
+        console.log(logData);
+    }
+  })
 
 const PORT = 3001;
-var logData = "";
+
+
 
 
 server.get('/', (req,res) => {
@@ -17,13 +43,15 @@ server.get('/', (req,res) => {
     console.log(logData);
 });
 server.post('/', (req,res) => {
-    res.send("You have hitted POST API on port: "+PORT);
-    let current = new Date();
-    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Someone hits the POST API \n";
-    fs.appendFile('./logs/APILogs.log',logData, (err) => {
-        if(err) throw err;
+    console.log(req.body);
+    clientDB.collection('employees').insert(req.body, function (err, result) {
+        if (err)
+           res.send('Error');
+        else
+          res.send('Success');
+  
     });
-    console.log(logData);
+    res.send("Requested body received");
 });
 
 server.put('/', (req,res) => {
@@ -48,5 +76,10 @@ server.delete('/', (req,res) => {
 
 
 server.listen(PORT, () => {
-    console.log("App is now answering to port: "+PORT);
+    let current = new Date();
+    logData = "[ "+current.toLocaleString()+" "+current.getTimezoneOffset()+" ] Application started and answering to port: "+PORT+" \n";
+    fs.appendFile('./logs/APILogs.log',logData, (err) => {
+        if(err) throw err;
+    });
+    console.log(logData);
 });
